03b02a3259e81725372d15a862fcc8b7e649ed62
Stephanie Viccari <stephviccari@gmail.com>
2018-10-09 13:08:04 -0400

    no need to test the time here

--- spec/features/user_views_upcoming_events_spec.rb
+++ spec/features/user_views_upcoming_events_spec.rb
@@ -9,27 +9,17 @@ RSpec.feature "user views details for the next event" do
       expect(page).to have_content("thoughtbot")
       expect(page).to have_content("41 Winter Street, 6th Floor")
       expect(page).to have_content("Boston")
-      expect(page).to have_content("6:00PM to 8:00PM")
     end
   end
 
   def stub_request_to_meetup_api
     stub_request(:get, /api.meetup.com/).
-      to_return(status: 200, body: events_json)
-  end
-
-  def events_json
-    [future_meetup_event].to_json
+      to_return(status: 200, body: [future_meetup_event].to_json )
   end
 
   def future_meetup_event
+    future_time_ms = 2.days.from_now.to_i * 1000
     event_data.merge("time" => future_time_ms)
-  end
-
-  def future_time_ms
-    date = 2.days.from_now
-    datetime = DateTime.new(date.year, date.month, date.day, 22, 0)
-    datetime.to_i * 1000
   end
 
   def event_data
